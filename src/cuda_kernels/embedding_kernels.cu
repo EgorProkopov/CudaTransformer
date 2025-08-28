7ea1a4ce3100abee38379e8bc53e3265d72078e4
EgorProkopov <egorprokopov216@gmail.com>
2025-08-28 12:48:29 +0300

    start embedding kernels

---
+++ src/cuda_kernels/embedding_kernels.cu
@@ -0,0 +1,26 @@
+// Embedding Layer kernels
+#include <cuda_runtime.h>
+
+#include <cuda_fp16.h>
+#include <cuda_bf16.h>
+
+
+// device kernels and funcitons
+template <typename scalar_t>
+__global__ void embedding_fwd_kernel(
+    const scalar_t* __restrict__ weights,
+    const int64_t vocab_size, const int64_t embedding_dim
+){
+
+}
+
+
+template <typename scalar_t>
+__global__ void embedding_bwd_kernel(
+    const scalar_t* __restrict__ weights,
+    const scalar_t* __restrict__ dy,
+    scalar_t* __restrict__ dweights,
+    const int64_t vocab_size, const int64_t embedding_dim
+){
+    
+}
