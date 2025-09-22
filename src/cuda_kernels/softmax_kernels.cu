76f6fb5ebda254e09300a60db227cec3d97b6c84
EgorProkopov <egorprokopov216@gmail.com>
2025-09-22 21:37:05 +0300

    add softmax wrappers

--- src/cuda_kernels/softmax_kernels.cu
+++ src/cuda_kernels/softmax_kernels.cu
@@ -1,7 +1,8 @@
 // includes 
-// #include <torch/extension.h>
+#include <torch/extension.h>
 #include <cuda_runtime.h>
 #include <c10/cuda/CUDAGuard.h>
+#include <ATen/cuda/CUDAContext.h>
 
 #include <math_constants.h>
 #include <cuda_fp16.h>
@@ -167,16 +168,116 @@ __global__ void safe_softmax_bwd_fp32_kernel(
 }
 
 // host functions
-// utils
+// forward
+static inline void check_device_forward(const torch::Tensor& x){
+    TORCH_CHECK(x.is_cuda(), "Input data (x) must be CUDA");
+}
 
+static inline void check_shape_forward(const torch::Tensor& x){
+    TORCH_CHECK(x.dim() == 2, "Input data (x) must be [batch_size, seq_len], but dim=", x.dim());
+}
+static inline void check_dtype_forward(const torch::Tensor& x){
+    TORCH_CHECK(x.scalar_type() == at::kFloat, "Input (x) must be float32, but got=", x.scalar_type());
+}
+
+
+// backward
+static inline void check_device_backward(
+    const torch::Tensor& y, const torch::Tensor& dy
+){
+    TORCH_CHECK(y.is_cuda(), "Input data (y) must be CUDA");
+    TORCH_CHECK(dy.is_cuda(), "Input data (dy) must be CUDA");
+
+    TORCH_CHECK(y.get_device() == dy.get_device(), "Input elements must be on the same device");
+}
+
+static inline void check_shape_backward(
+    const torch::Tensor& y, const torch::Tensor& dy
+){
+    TORCH_CHECK(y.dim() == 2, "Input data (y) must be [batch_size, seq_len]");
+    TORCH_CHECK(dy.dim() == 2, "Input data (dy) must be [batch_size, seq_len]");
+
+    TORCH_CHECK(y.sizes() == dy.sizes(), "Input data (y and dy) sizes mismatch");
+}
+
+static inline void check_dtype_backward(
+    const torch::Tensor& y,
+    const torch::Tensor& dy
+){
+    TORCH_CHECK(
+        y.scalar_type() == at::kFloat, 
+        "Input (y) must be float32, but got=", y.scalar_type()
+    );
+    TORCH_CHECK(
+        dy.scalar_type() == at::kFloat, 
+        "Input (dy) must be float32, but got=", dy.scalar_type()
+    );
+}
 
 
 // torch forward wrapper
+torch::Tensor safe_softmax_forward_fp32(torch::Tensor x, float eps){
+    c10::cuda::CUDAGuard device_guard(x.device());
+    check_device_forward(x);
+    check_shape_forward(x);
+    check_dtype_forward(x);
 
+    auto x_c = x.contiguous();
+    
+    const auto batch_size = x_c.size(0);
+    const auto seq_len = x_c.size(1);
+
+    auto y = torch::empty_like(x_c);
+
+    const int warp_size = 32;
+    const int warps_per_block = 4;
+    const int threads = warp_size * warps_per_block;
+    const int num_blocks = (batch_size + warps_per_block -1) / warps_per_block;
+
+    auto stream at::cuda::getCurrentCUDAStream();
+
+    safe_softmax_fwd_fp32_kernel<<<num_blocks, threads, 0, stream.stream()>>>(
+        x_c<float>.data_ptr(),
+        y<float>.data_ptr(),
+        batch_size, seq_len, eps
+    );
+    
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+    return y;
+}
 
 
 // torch backward wrapper
+torch::Tensor safe_softmax_backward_fp32(torch::Tensor y, torch::Tensor dy){
+    c10::cuda::CUDAGuard device_guard(y.device());
+    check_device_forward(y, dy);
+    check_shape_forward(y, dy);
+    check_dtype_backward(y, dy);
 
+    auto y_c = y.contiguous();
+    auto dy_c = dy.contiguous();
+
+    const auto batch_size = y_c.size(0);
+    const auto seq_len = y_c.size(0);
+    
+    auto dx = torch::empty_like(x_c);
+    
+    const int warp_size = 32;
+    const int warps_per_block = 4;
+    const int threads = warp_size * warps_per_block;
+    const int num_blocks = (batch_size + warps_per_block - 1) / warps_per_block;
+
+    auto stream at::cuda::getCurrentCUDAStream();
+
+    safe_softmax_bwd_fp32_kernel<<<num_blocks, threads, 0, stream.stream()>>>(
+        y_c<float>.data_ptr(),
+        dy_c<float>.data_ptr(),
+        dx<float>.data_ptr(),
+        batch_size, seq_len
+    );
+    C10_CUDA_KERNEL_LAUNCH_CHECK();
+    return dx;
+}
 
 
 // bindings
