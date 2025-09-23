2e331cc2b54ed9f41d973fed38dd316816b8be88
EgorProkopov <egorprokopov216@gmail.com>
2025-09-23 16:40:58 +0300

    softmax benchmarks base

--- src/benchmarks/softmax_benchmarks.py
+++ src/benchmarks/softmax_benchmarks.py
@@ -1,10 +1,36 @@
+from typing import Optional
+
 import torch
 
 from src.modules.softmax import SafeSoftmax
 
 
-def torch_softmax_forward_benchmarks(x: torch.Tensor):
+WARM_UP_RUNS = 30
+ITERATIONS = 200
+
+
+def torch_softmax_warpup():
+    pass
+
+
+def custom_softmax_warmup():
+    pass
+
+
+def torch_softmax_forward_benchmarks(x: torch.Tensor, return_res=True) -> tuple[float, Optional[torch.Tensor]]:
     """
-    Compute torch softmax benchmark and
+    Compute torch forward softmax benchmark (cpu and cuda)
     """
-    
+    pass
+
+
+def torch_softmax_backward_benchmarks(dy: torch.Tensor, y: torch.Tensor, return_res=True) -> tuple[float, Optional[torch.Tensor]]:
+    pass
+
+
+def custom_softmax_forward_benchmarks(x: torch.Tensor, return_res=True) -> tuple[float, Optional[torch.Tensor]]:
+    pass
+
+
+def custom_softmax_backward_benchmarks(dy: torch.Tensor, y: torch.Tensor, return_res=True) -> tuple[float, Optional[torch.Tensor]]:
+    pass
