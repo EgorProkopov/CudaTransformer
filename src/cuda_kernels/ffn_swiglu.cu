7f9d85e9bd0d537beead90bea3dbee77829a1fba
EgorProkopov <egorprokopov216@gmail.com>
2025-09-24 14:01:13 +0300

    add ffn naive 1st kernel

--- src/cuda_kernels/ffn_swiglu.cu
+++ src/cuda_kernels/ffn_swiglu.cu
@@ -4,6 +4,7 @@
 #include <ATen/cuda/CUDAContext.h>
 
 #include <math_constants.h>
+#include <curand_kernel.h>
 #include <cuda_fp16.h>
 #include <cuda_bf16.h>
 
@@ -12,15 +13,132 @@
 
 
 
-// 1st fwd-kernel: GEMM + SwiGLU + Dropout
-__global__ void ffn_swiglu_dropout_fp32_kernel(
-    const float* __restrict__ x,    // input data to ffn block
-    float* __restrict__ z,          // output data
 
-    const float* __restrict__ W_1,  // [W_gate | W_in]
-    const float* __restrict__ b_1,  // [b_gate | b_in]
+// ===========================================================================
+// =============================== v1 ========================================
+// ===========================================================================
+// 1st fwd-kernel v1: GEMM + SwiGLU + Dropout naive
+__global__ void ffn_swiglu_dropout_fp32_kernel_v1(
+    const float* __restrict__ x,           // input data to ffn block
+    float* __restrict__ z,                 // output data
 
-    const uint32_t num_vectors,     // batch_size * seq_len      
+    const float* __restrict__ W_gate,      // weights for gating layer
+    const float* __restrict__ b_gate,      // bias for gating layer
+
+    const float* __restrict__ W_in,        // weights for input layer
+    const float* __restrict__ b_in,        // bias for input layer
+
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len      
+
+    // vector shapes
+    const uint32_t embedding_dim,
+    const uint32_t hidden_dim
+){
+    // row and col are calculated for result matrix with shape (num_vectors, hidden_dim)
+    const uint32_t row = threadIdx.y + blockDim.y * blockIdx.y;
+    const uint32_t col = threadIdx.x + blockIdx.x * blockDim.x;
+
+    const uint64_t x_offset = (uint64_t)row * (uint64_t)embedding_dim;
+    const uint64_t z_index = (uint64_t)row * (uint64_t)hidden_dim + col;
+
+    if (row < num_vectors && col < hidden_dim){
+        float sum_gate = 0.0f;
+        float sum_in = 0.0f;
+        #pragma unroll 1
+        for (uint32_t k = 0; k < embedding_dim; k++){
+            float x_value = x[x_offset + k];
+            uint64_t w_index = (uint64_t)k * hidden_dim + col;
+            sum_gate = fmaf(x_value, W_gate[w_index], sum_gate);
+            sum_in = fmaf(x_value, W_in[w_index], sum_in);
+        }
+        sum_gate += b_gate[col];
+        sum_in += b_in[col];
+        
+        // SwiGLU:
+        // y = (x @ W_gate + b_gate) * sigmoid(x @ W_gate + b_gate) * (x @ W_in + b_in)
+            
+        // if z_gate = x @ W_gate + b_gate
+        // if z_in = x @ W_in + b_in
+        // and z = [z_gate | z_in]
+        // then:
+        // y = z_gate * sigmoid(z_gate) * z_in
+
+        // sigmoid = exp(x) / (1 + exp(x)) or 1 / (1 + exp(-x))
+        // chose 1st for numerical stability
+        float sigmoid = __fdividef(__expf(sum_gate), 1 + __expf(sum_gate));
+        float value = sum_gate * sigmoid * sum_in;
+
+        if (mask && p > 0.0f){
+            curandStatePhilox4_32_10_t st;
+            curand_init(
+                (uint64_t)seed,
+                z_index,
+                (uint64_t)offset,
+                &st
+            );
+            float r = curand_uniform(&st);
+            uint8_t m = (r > p);
+            mask[z_index] = m;
+
+            float inv_dropout = 1.0f / (1.0f - p);
+            if (mask){
+                z[z_index] = value * inv_dropout;
+            } else {
+                z[z_index] = 0.0f;
+            }
+        } else {
+            z[z_index] = value;
+        }
+    }
+}
+
+
+// 2st fwd-kernel v2: GEMM + Residual + Dropout naive
+__global__ void ffn_residual_dropout_fp32_kernel_v1(
+    const float* __restrict__ residual,    // input data to ffn block
+    const float* __restrict__ z,           // input data from prev kernel
+    float* y,                              // output data
+
+    const float* __restrict__ W_2,         // second ffn layer weights
+    const float* __restrict__ b_2,         // second ffn layer bias
+
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len
+
+    // vector shapes
+    const uint32_t embedding_dim,
+    const uint32_t hidden_dim
+){
+
+}
+
+// ===========================================================================
+// =============================== v2 ========================================
+// ===========================================================================
+
+// 1st fwd-kernel v2: GEMM + SwiGLU + Dropout with coalesced access
+__global__ void ffn_swiglu_dropout_fp32_kernel_v2(
+    const float* __restrict__ x,           // input data to ffn block
+    float* __restrict__ z,                 // output data
+
+    const float* __restrict__ W_1,         // [W_gate | W_in]
+    const float* __restrict__ b_1,         // [b_gate | b_in]
+
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len      
 
     // vector shapes
     const uint32_t embedding_dim,
@@ -30,16 +148,21 @@ __global__ void ffn_swiglu_dropout_fp32_kernel(
 }
 
 
-// 2st fwd-kernel: GEMM + residual + dropout
-__global__ void ffn_residual_dropout_fp32_kernel(
-    const float* __restrict__ x,    // input data to ffn block
-    const float* __restrict__ z,    // input data from prev kernel
-    float* y,                       // output data
+// 2st fwd-kernel v2: GEMM + Residual + Dropout with coalesced access
+__global__ void ffn_residual_dropout_fp32_kernel_v2(
+    const float* __restrict__ residual,    // input data to ffn block
+    const float* __restrict__ z,           // input data from prev kernel
+    float* y,                              // output data
 
-    const float* __restrict__ W_2,  // second ffn layer weights
-    const float* __restrict__ b_2,  // second ffn layer bias
+    const float* __restrict__ W_2,         // second ffn layer weights
+    const float* __restrict__ b_2,         // second ffn layer bias
 
-    const uint32_t num_vectors,     // batch_size * seq_len
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len
 
     // vector shapes
     const uint32_t embedding_dim,
@@ -48,6 +171,56 @@ __global__ void ffn_residual_dropout_fp32_kernel(
 
 }
 
+
+// ===========================================================================
+// =============================== v3 ========================================
+// ===========================================================================
+
+// 1st fwd-kernel v3: GEMM + SwiGLU + Dropout with SMEM tiling
+__global__ void ffn_swiglu_dropout_fp32_kernel_v3(
+    const float* __restrict__ x,           // input data to ffn block
+    float* __restrict__ z,                 // output data
+
+    const float* __restrict__ W_1,         // [W_gate | W_in]
+    const float* __restrict__ b_1,         // [b_gate | b_in]
+
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len      
+
+    // vector shapes
+    const uint32_t embedding_dim,
+    const uint32_t hidden_dim
+){
+
+}
+
+
+// 2st fwd-kernel v3: GEMM + Residual + Dropout with SMEM tiling
+__global__ void ffn_residual_dropout_fp32_kernel_v3(
+    const float* __restrict__ residual,    // input data to ffn block
+    const float* __restrict__ z,           // input data from prev kernel
+    float* y,                              // output data
+
+    const float* __restrict__ W_2,         // second ffn layer weights
+    const float* __restrict__ b_2,         // second ffn layer bias
+
+    uint8_t* __restrict__ mask,            // dropout mask
+    const float p,                         // dropout rate
+    const uint32_t seed,                   // dropout seed
+    const uint32_t offset,                 // rnd offset
+
+    const uint32_t num_vectors,            // batch_size * seq_len
+
+    // vector shapes
+    const uint32_t embedding_dim,
+    const uint32_t hidden_dim
+){
+
+}
 
 // backward-pass kernel
 
