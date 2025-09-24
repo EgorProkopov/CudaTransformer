142f05fb176a510a1d810b4baa8be508648bd0d1
EgorProkopov <egorprokopov216@gmail.com>
2025-09-24 15:56:50 +0300

    ffn naive 1st fwd kernel fixes and 2nd fwd kernel

--- src/cuda_kernels/ffn_swiglu.cu
+++ src/cuda_kernels/ffn_swiglu.cu
@@ -68,25 +68,24 @@ __global__ void ffn_swiglu_dropout_fp32_kernel_v1(
         // then:
         // y = z_gate * sigmoid(z_gate) * z_in
 
-        // sigmoid = exp(x) / (1 + exp(x)) or 1 / (1 + exp(-x))
-        // chose 1st for numerical stability
-        float sigmoid = __fdividef(__expf(sum_gate), 1 + __expf(sum_gate));
-        float value = sum_gate * sigmoid * sum_in;
+        // sigmoid = 1 / (1 + exp(-x))
+        float sigmoid = __fdividef(1, 1 + __expf(-sum_gate));
+        float value = (sum_gate * sigmoid) * sum_in;
 
         if (mask && p > 0.0f){
-            curandStatePhilox4_32_10_t st;
+            curandStatePhilox4_32_10_t st;  // unique state for random number generator
             curand_init(
                 (uint64_t)seed,
                 z_index,
                 (uint64_t)offset,
                 &st
-            );
+            );                              // rng initalization
             float r = curand_uniform(&st);
             uint8_t m = (r > p);
             mask[z_index] = m;
 
-            float inv_dropout = 1.0f / (1.0f - p);
-            if (mask){
+            float inv_dropout = 1.0f / (1.0f - p);  // to avoid math expectation change
+            if (m){
                 z[z_index] = value * inv_dropout;
             } else {
                 z[z_index] = 0.0f;
@@ -104,8 +103,8 @@ __global__ void ffn_residual_dropout_fp32_kernel_v1(
     const float* __restrict__ z,           // input data from prev kernel
     float* y,                              // output data
 
-    const float* __restrict__ W_2,         // second ffn layer weights
-    const float* __restrict__ b_2,         // second ffn layer bias
+    const float* __restrict__ W_out,       // second ffn layer weights
+    const float* __restrict__ b_out,       // second ffn layer bias
 
     uint8_t* __restrict__ mask,            // dropout mask
     const float p,                         // dropout rate
@@ -118,7 +117,41 @@ __global__ void ffn_residual_dropout_fp32_kernel_v1(
     const uint32_t embedding_dim,
     const uint32_t hidden_dim
 ){
+    const uint32_t row = threadIdx.y + blockDim.y * blockIdx.y;
+    const uint32_t col = threadIdx.x + blockIdx.x * blockDim.x;
 
+    const uint64_t z_offset = (uint64_t)row * (uint64_t)hidden_dim;
+    const uint64_t y_index = (uint64_t)row * (uint64_t)embedding_dim + col;
+
+    if (row < num_vectors && col < embedding_dim){
+        float sum = 0.0f;
+
+        #pragma unroll 1
+        for (uint32_t k = 0; k < hidden_dim; k++){
+            sum = fmaf(z[z_offset + k], W_out[k * embedding_dim + col], sum);
+        }
+
+        float value = sum + residual[y_index];
+        if (mask && p > 0.0f){
+            curandStatePhilox4_32_10_t st;
+            curand_init(
+                (uint64_t)seed,
+                y_index,
+                (uint64_t)offset,
+                &st
+            );
+            float r = curand_uniform(&st);
+            uint8_t m = (r > p);
+            mask[y_index] = m;
+
+            float inv_dropout = 1.0f / (1.0f - p);
+            if (m){
+                y[y_index] = value * inv_dropout;
+            } else {
+                y[y_index] = 0.0f;
+            }
+        }
+    }
 }
 
 // ===========================================================================
